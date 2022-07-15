fff7fc4a37ed086eb1613446e824ad691306d6a1
Ananthan <ananthan@hotmail.ca>
2022-07-15 10:06:57 -0400

    initial comit

---
+++ .history/indesx_20220715094851.js
@@ -0,0 +1,15 @@
+const fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+const stageEl = document.getElementById("stage")
+const fightButton = document.getElementById("fightButton")
+
+const stage = document.getElementById("stage")
+
+fightButton.addEventListener("click", function() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    
+    
+    
+})
